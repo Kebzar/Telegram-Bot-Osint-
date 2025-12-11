f472fa4f9bf2cc3ed4e7f03ca55d48f8a3353914
Kebzar <kebzar@protonmail.com>
2025-12-11 13:51:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -225,7 +225,7 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY,
     username TEXT,
-    balance REAL DEFAULT 10.0,
+    balance REAL DEFAULT 4,
     searches INTEGER DEFAULT 0,
     registration_date TEXT DEFAULT CURRENT_TIMESTAMP,
     subscription_type TEXT DEFAULT 'free',
